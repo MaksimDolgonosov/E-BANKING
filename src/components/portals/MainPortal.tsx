0358687a9b6573d8de103752030ee36208ad62fd
MAKSIM <max_air@bk.ru>
2024-09-26 16:56:31 +0300

    doing portal

---
+++ src/components/portals/MainPortal.tsx
@@ -0,0 +1,46 @@
+import { useState, useLayoutEffect, ReactNode } from "react";
+import { createPortal } from "react-dom";
+//import { deflate } from "zlib";
+import "./mainPortal.scss";
+
+
+
+
+interface IPortalProps {
+    children: ReactNode,
+    wrapperId?: string
+}
+
+function createElementAndAppendToBody(id: string) {
+    const wrapperElement = document.createElement("div");
+    wrapperElement.setAttribute("id", id);
+    wrapperElement.setAttribute("class", "modal");
+    document.body.append(wrapperElement);
+    return wrapperElement;
+}
+
+const MainPortal = ({ children, wrapperId = "mainPortal-wrapper" }: IPortalProps) => {
+    const [wrapperElement, setWrapperElement] = useState<HTMLElement | null>(null);
+
+    useLayoutEffect(() => {
+        let element = document.getElementById(wrapperId);
+        let created = false;
+        if (!element) {
+            created = true;
+            element = createElementAndAppendToBody(wrapperId)
+            setWrapperElement(element)
+        }
+
+        return () => {
+            if (created) {
+                element?.remove();
+            }
+        }
+
+    }, [wrapperId])
+
+    if (wrapperElement === null) return null;
+    return createPortal(children, wrapperElement);
+}
+
+export default MainPortal;
