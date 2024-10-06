3c9a0952daee2c55923826d3da77b179b1130cce
MAKSIM <max_air@bk.ru>
2024-10-06 19:21:33 +0300

    loginPage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.scss';
 import MainPage from "../../pages/Main/Main";
-
+import LoginPage from "../../pages/LoginPage/LoginPage";
 function App() {
 
   return (
     <BrowserRouter>
      <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
     
     </BrowserRouter>
